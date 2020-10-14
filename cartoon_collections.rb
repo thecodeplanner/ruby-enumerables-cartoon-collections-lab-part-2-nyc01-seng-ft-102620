ac0d565b7ae907c9a7e1d42ecdef8f76d8de02d3
Melissa <thecodeplanner@gmail.com>
2020-10-14 23:46:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -59,7 +59,7 @@ end
  def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |call|
-    call == valid_calls 
+    call == valid_calls.each  
   end
 end 
       
